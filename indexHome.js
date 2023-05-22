f9293d97f84741be355a52fc4adccd07b84d3901
Sara Viana <112579669+Saraoliveiraviana@users.noreply.github.com>
2023-05-22 11:06:06 -0300

    Atualizaçao
    Atualização

---
+++ indexHome.js
@@ -0,0 +1,26 @@
+function onclick_ir()
+{
+
+    window.open('https://github.com/Lucas-Saraiva/APS_Mobilidade_Urbana')
+
+}
+
+function limpar_componentes()
+{
+
+    document.getElementById("validationDefault01").value = "";
+    document.getElementById("sel_estado").value = "";
+    document.getElementById("validationDefault03").value = "";
+    document.getElementById("validationDefault04").value = "";
+
+}
+function enviar_dados()
+{ }
+
+function onclick_enviar()
+{
+
+    limpar_componentes()
+    enviar_dados()
+
+}
